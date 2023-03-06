91016fc8d7381f7ea26c2f1941721fd04250f7e5
kaliloaida <kaliloaida@gmail.com>
2023-03-06 14:10:54 +0600

    header navigation and click function

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./Home.css";
 import Profile from "./Profile/Profile";
 import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 
 export default function Home() {
   return (
     <div className="home-container">
       <Profile />
       <Footer />
+      <Header/>
     </div>
   );
 }
